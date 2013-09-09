fcbb51a6031a430cc5d878fe117a7e13fdac31b0
angleman <angleman@cormea.com>
2013-09-09 17:31:34 -0600

    initial stub

---
+++ maxminded.js
@@ -0,0 +1,9 @@
+var maxmind    = require('maxmind')        // runk/node-maxmind
+  , loader     = require('maxmind-loader') // angleman/maxmind-loader
+  , geos       = require('geos-major')     // angleman/geos-major
+;
+
+
+
+
+module.exports = maxmind;
