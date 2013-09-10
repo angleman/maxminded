42dfdf172963f8dd49b3efc1cd75eacb8c0a6d76
angleman <angleman@cormea.com>
2013-09-10 15:11:54 -0600

    version bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,5 @@
 module.exports = function(grunt){
 
-grunt.initConfig({
-  license: {
-    options: {
-    	output: 'LICENSES'
-    },
-    your_target: {
-    	output: 'LICENSES2'
-    },
-  },
-})
-
   grunt.loadNpmTasks('grunt-bump');
-  grunt.loadNpmTasks('grunt-license');
 
 };
