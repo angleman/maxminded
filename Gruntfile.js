33c5b71c7f4c9c22bd9ce7b8768696797f0767bf
angleman <angleman@cormea.com>
2013-09-10 15:09:31 -0600

    Travis CI support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
 module.exports = function(grunt){
 
+grunt.initConfig({
+  license: {
+    options: {
+    	output: 'LICENSES'
+    },
+    your_target: {
+    	output: 'LICENSES2'
+    },
+  },
+})
+
   grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-license');
 
 };
