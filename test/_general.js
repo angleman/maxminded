59553a309182dfca8121b7cf976ae388a805459d
angleman <angleman@cormea.com>
2013-09-12 12:55:48 -0600

    add load test with prior run cleanup

---
+++ test/_general.js
@@ -0,0 +1,21 @@
+// test/main.js
+var should = require('should')
+  , fs     = require('fs')
+;
+
+// clean up from prior run
+if (fs.existsSync('/tmp/GeoLiteCity.dat')) {
+	fs.unlinkSync('/tmp/GeoLiteCity.dat'));
+}
+if (fs.existsSync('/tmp/GeoLiteCity.dat.gz')) {
+	fs.unlinkSync('/tmp/GeoLiteCity.dat.gz'));
+}
+
+describe('package', function() {
+    describe('should', function() {
+    	var maxminded;
+    	it('load', function() {
+	        maxminded = require('../maxminded.js');
+        });
+    });
+});
